b4cf5e295d718f1d78403ce6dbc5421273e08421
Rachna <rachnakum0476@gmail.com>
2023-01-20 15:21:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import {useEffect, useState} from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const apiKey = "fe7d80a51c2618e23cdb66cfa44e63d5"
+  const[inputCity,setInputCity] = useState("")
+  const [data, setData] = useState({})
+  const getWeatherDetails=(cityName) =>{
+    if(!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid="+ apiKey
+    axios.get(apiURL).then((res) =>{
+      console.log("response",res)
+      setData(res.data)
+    }).catch((err) =>{
+      console.log("err",err)
+    })
+  }
+  const handleChangeInput=(e) =>{
+    console.log("value", e.target.value)
+ setInputCity(e.target.value)
+  }
+  const handleSearch = () =>{
+    getWeatherDetails(inputCity)
+  }
+ 
+  return(
+    <div className="col-md-12">
+    <div className="weatherbg">
+    <h1 className="heading">Weather App</h1>
+    <div className="d-grid gap-3 col-4 mt-4">
+    <input type="text" className="form-control"
+    value={inputCity}
+     onChange={handleChangeInput}/>
+    <button className="btn btn-primary" type="button"
+    onClick={handleSearch}
+    >Search</button>
+    </div>
+    </div>
+    {Object.keys(data).length>0 &&
+    <div className="col-md-12 text-center mt-5">
+    <div className="shadow rounded weathrResultBox">
+    <img className="weatherIcon" src="https://eshop.macsales.com/blog/wp-content/uploads/2019/02/ios12-weather-app-icon-thumbnail_2x.png" />
+    <h5 className="weatherCity">{data?.name}
+    </h5>
+    <h6 className="weatherTemp">{((data?.main?.temp)-273.15).toFixed(2)}°C</h6>
+    </div>
+    </div>
+    }
     </div>
   );
 }
-
 export default App;
